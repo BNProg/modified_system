859988cd3e49b38ee452e3c5bcdc11810253c212
BNProg <98820403+BNProg@users.noreply.github.com>
2022-02-06 11:41:48 +0300

    Update lime_image.py

--- lime/lime_image.py
+++ lime/lime_image.py
@@ -142,6 +142,8 @@ class LimeImageExplainer(object):
         self.model_input_1_data_samples_number = 1280
         self.rgb_image_data = []
         self.ela_image_data = []
+        self.input_1_dim = None
+        self._input_2_dim = None
         
     def explain_instance(self, image, classifier_fn, labels=(1,),
                          hide_color=None,
@@ -292,6 +294,8 @@ class LimeImageExplainer(object):
                     self.image_sequences = np.array(imgs)
                     model_input_1 = self.generate_input_1_data(np.array(imgs))
                     model_input_2 = self.process_model_input_2_data(np.array(imgs), text)
+                    self.input_1_dim = model_input_1
+                    self.input_2_dim = model_input_2
                     preds = classifier_fn([model_input_1, model_input_2]) 
                     #preds = classifier_fn([input_1, input_2])
                     labels.extend(preds)
@@ -306,6 +310,8 @@ class LimeImageExplainer(object):
                     self.image_sequences = np.array(imgs)
                     model_input_1 = self.generate_input_1_data(np.array(imgs))
                     model_input_2 = self.process_model_input_2_data(np.array(imgs), text)
+                    self.input_1_dim = model_input_1
+                    self.input_2_dim = model_input_2
                     preds = classifier_fn([model_input_1, model_input_2]) 
                     #preds = classifier_fn([input_1, input_2])
                     labels.extend(preds)
